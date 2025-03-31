79accd4c692517ad1f1d1237baca6d400b17056a
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2025-03-31 19:55:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ setInterval(function relog() {
 // Exibição da data em Alemão
 function exibirDataAtualizada() {
     let meses = [ "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
-    let semanas = [ "Samstag", "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"];
+    let semanas = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
     
     let data = new Date();
     // Adiciona 23 segundos
